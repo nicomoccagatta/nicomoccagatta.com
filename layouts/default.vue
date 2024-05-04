b6240418abef4cb736d155f4165dc2450f729b33
Nico Moccagatta <nicomoccagatta@gmail.com>
2024-05-04 16:49:36 -0300

    SEO: og + enhancements

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,32 @@
+<script lang="ts" setup>
+useSeoMeta({
+  ogImage: "https://nicomoccagatta.com/img/nico.png",
+  ogType: 'profile',
+  ogUpdated_time: new Date().toISOString(),
+  profileFirstName: 'Nicolas',
+  profileGender: 'male',
+  profileLastName: 'Moccagatta',
+  profileUsername: 'nicomoccagatta@gmail.com',
+  twitterTitle: "Nicolas Moccagatta's Personal Website",
+  twitterDescription: "Hello, I'm Nicolas Moccagatta! I'm a seasoned Software Engineer with over 7 years of experience in Full Stack Software Development, specializing in Frontend Web Technologies.",
+  twitterImage: "https://nicomoccagatta.com/img/nico.png",
+  twitterCard: "summary_large_image",
+});
+
+useHeadSafe({
+  htmlAttrs: {
+    lang: "en"
+  },
+  link: [
+    {
+      href: "/favicon.ico",
+      rel: "icon",
+      type: "image/x-icon",
+    },
+  ],
+});
+</script>
+
 <template>
   <div class="container">
     <div class="min-h-screen flex flex-col overflow-x-hidden py-5">
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,10 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/seo', "@nuxtjs/tailwindcss", '@nuxtjs/color-mode'],
   site: {
-    url: "https://nicomoccagatta.com/",
+    url: "https://nicomoccagatta.com",
     name: "Nicolas Moccagatta's Personal Website",
-    description: "Hello, I'm Nicolas Moccagatta! I'm a seasoned Software Engineer with over 7 years of experience in Full Stack Software Development, specializing in Frontend Web Technologies. Holding a Bachelor's degree in Engineering from the University of Buenos Aires, where I majored in Computer Science",
-    defaultLocale: "en",
+    description: "Hello, I'm Nicolas Moccagatta! I'm a seasoned Software Engineer with over 7 years of experience in Full Stack Software Development, specializing in Frontend Web Technologies.",
+    defaultLocale: "en_US",
   },
   tailwindcss: {
     cssPath: '~/assets/css/tailwind.scss',
